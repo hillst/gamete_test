7f9b2e98f23d8d09f08bf049904a951700d7230e
Steven Hill <hill2steve@gmail.com>
2016-10-24 14:21:20 -0400

    contains code for computing the naive histogram.

--- TODO.md
+++ TODO.md
@@ -3,3 +3,12 @@
     - accepts four counts
     - how many values allowed per value?
     - how do we signal cases with more than one value?
+
+3) ok bettr idea
+    - we are gonna let this go on the fly while we evaluate stuff but maybe all at once
+    - compute histogram on the fly (n^2) runtime m space (m is small).
+        - histogram of 1 2 3 4 gametes for each pair
+        - we are just producing a count of n_cells x 4 table
+            n_obs x n-gametes essentially
+            1,2,3,4,5,6,7 obs => 1, 2, 3 ,4 gametes
+    - ultimately we are going to need to reuse these functions to compute probabilities before filtering -- it might make more sense to make them distinct functions.
--- src/gamete_test.cpp
+++ src/gamete_test.cpp
@@ -97,21 +97,24 @@ class CSVIterator{
         CSVRow m_row;
 };
 
-void compute_gamete_stats(CSVRow* i, CSVRow* j, int * res){
+int compute_gamete_stats(CSVRow* i, CSVRow* j, int * res){
     /*
      * Returns a count vector for the possible haplotypes (00,01,10,11)
      */
     for (int k = 0; k < 4; k++){ res[k]=0; }
-
+    int count = 0;
     for (int k = 1; k < i->size(); k++){ // start at 1 to skip the site-name column
         // im leaving you a puzzle
         int a = ((*i)[k].c_str()[0] - 48); // 0 = 0, 1 = 1, 2 = missing
         int b = ((*j)[k].c_str()[0] - 48); // im a bitmaster
-
+        if (!(b>>1)){
+            count++;
+        }
         if (!((a >> 1) | (b >> 1))){
             res[(a << 1) + b]++;
         }
     }
+    return count;
 
 }
 
@@ -132,14 +135,19 @@ void print_aggregate(vector<float *> vals, vector<string> site_names){
         }
         cout << endl;
     }
-    
+}
+
+void print_hist(int ** hist, int size_i){
+    for (int i = 0; i < size_i; i++){
+        for(int j =0;j<4;j++){
+            cout << hist[i][j] << " ";
+        }
+        cout << endl;
+    }
+
 }
 
 int main(int argc, char** argv){
-    //ifstream file("shm_normal_B.fixed.csv"); // full data
-//    ifstream file("shm_normal_B.X.csv"); // x chromosome only
-//    ifstream file("shm_normal_B_10k.csv"); //slowest test
-
     ifstream file(argv[1]); //fastest test
     vector<CSVRow> data;
     cerr << "Loading data..." << endl;
@@ -148,7 +156,7 @@ int main(int argc, char** argv){
     for(CSVIterator loop(file); loop != CSVIterator(); ++loop){
         if (idx == 0){
             idx++;
-        } else if ((*loop).count_present() < MIN_OBS){
+        } else if ((*loop).count_present() <= MIN_OBS){
             continue;
         } else{
             data.push_back((*loop));
@@ -156,12 +164,24 @@ int main(int argc, char** argv){
         }
     }
     int * result = new int[4];
+    // we can have a bunch of functions for our prepare and our filtering pipeline
 
-    vector<float*> results;    
+    vector<int*> results;    
     time_t start = time(NULL);
     // we are still looking to get faster, we need speedup from somewhere of about 1 order of magnitude
     //  maybe we can get it from unrolling the loop? are i and j continugous in memory?
     cerr << "processing "<< data.size() << " sites" << endl;
+    int valid_pairs = 0;
+    int count;
+    
+    auto hist = new int[data[0].size()][4];
+    for (int i = 0; i < data[0].size(); i++){
+        for (int j = 0; j < 4; j++){
+            hist[i][j] = 0;
+        }
+    }
+
+
     for (int i = 0; i < data.size(); i++){
         CSVRow left = data.at(i);
         //float * aggregate = new float[4];
@@ -169,43 +189,46 @@ int main(int argc, char** argv){
         //working on this mess -- modularize this.
         //for (int k = 0; k < 4; k++){ aggregate[k] = 0; }
         
-        float zz = 0; float zo = 0; float oz = 0; float oo =0;
-        #pragma omp parallel for reduction(+:zz,zo,oz,oo)
+        //float zz = 0; float zo = 0; float oz = 0; float oo =0;
+        #pragma omp parallel for reduction(+:count)
         for (int j = 0; j < data.size(); j++){
             float total = 0;
             int * result = new int[4];
-            compute_gamete_stats(&left, &data.at(j), result);
+            int count = compute_gamete_stats(&left, &data.at(j), result); // count is n_miss
             //we could be missing cache here
+            int n_alleles = 0;
             for (int k = 0; k < 4; k++){
-                total += result[k];
+                if (result[k] > 0){
+                    n_alleles += 1;
+                }
+                total += result[k]; // n_observations
             }
-            if (total != 0){ // ignores it in the case our denomenator is 0 (this is desired behavior)
-                zz += result[0] / total;
-                zo += result[1]/ total;
-                oz += result[2]/total;
-                oo += result[3]/total;
+
+            if (total >= 4){ // min 4 pairs is our threshold 
+                hist[count][n_alleles-1]++; // alleles are in 1,2,3,4 so -1
+                                            // count is off by one (which is what we want)
             }
             delete [] result;
             // now they are all the same so we should probably try just counting first
         }
-        float total = 0;
-        total = zz + zo + oz + oo;
-        zz /= total;
-        zo /= total; 
-        oz /= total; 
-        oo /= total;
-        //for (int k = 0; k < 4; k++){ total+=aggregate[k];}
-        //for (int k = 0; k < 4; k++){ aggregate[k] /= total; }
-        float * aggregate = new float[4];
-        aggregate[0] = zz; aggregate[1] = zo; aggregate[2] = oz; aggregate[3] = oo; 
-        results.push_back(aggregate);
+     //   results.push_back(total);
+
         cerr << "processed outer " << i << " " << (i+1) / (time(NULL) - start + .0001) <<  " sites/second \r";
+    }
+
+    //print histogram routine 
+    for (int i =0; i < data[0].size(); i++){
+        for ( int j = 0; j < 4; j++){
+            cout << hist[i][j] << " ";
+        }   
+        cout << endl;
     }
     
     cerr << "\nprocessed at: " << (data.size()) / (time(NULL) - start + .0001) <<  " sites/second \n";  
     cerr << "elapsed: " << time(NULL) - start << endl;
     cerr << "elapsed: " << (time(NULL) - start) / 1000<< endl;
-    print_aggregate(results, site_lookup);
+    cout << "count: " << count << endl;
+    //print_aggregate(results, site_lookup);
 }
 
 
